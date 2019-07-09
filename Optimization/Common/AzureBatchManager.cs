277d441008b14078c652eaa6930acd50672e4f0a
IlshatGaripov <igjump@gmail.com>
2019-07-09 17:57:28 +0300

    Remove Optimization.Example

--- Optimization/Common/AzureBatchManager.cs
+++ Optimization/Common/AzureBatchManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.Batch;
